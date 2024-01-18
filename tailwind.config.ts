8a71233071faa51fbb283d4c8f87f11cf0bbb626
thewanionly <pelloani@gmail.com>
2024-01-19 05:05:13 +0800

    feat: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      green: '#a4ffaf',
+      red: '#f64a4a',
+      orange: '#fb7c58',
+      yellow: '#f8cd65',
+      'grey-light': '#e6e5ea',
+      grey: '#817d92',
+      'grey-dark': '#24232c',
+      'grey-darkest': '#18171f',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       fontFamily: {
         'jetbrains-mono': ['var(--font-jetbrains-mono)'],
       },
