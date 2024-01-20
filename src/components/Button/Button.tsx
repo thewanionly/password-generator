b6e48786c7154335ed930733e5dccc70a1775861
thewanionly <pelloani@gmail.com>
2024-01-21 00:07:59 +0800

    feat: update Button styles to match the designs

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,19 @@
-export const Button = ({ label }: { label: string }) => {
+import { twMerge } from 'tailwind-merge';
+
+type ButtonProps = React.ComponentProps<'button'> & {
+  /* Button's label */
+  label: string;
+};
+
+export const Button = ({ className = '', label, ...props }: ButtonProps) => {
   return (
-    <button type="button" className=" bg-green px-4 py-2 text-grey-dark">
+    <button
+      type="button"
+      className={twMerge(
+        `border-2 border-green bg-green px-16 py-5 uppercase text-grey-dark hover:bg-grey-dark hover:text-green ${className}`
+      )}
+      {...props}
+    >
       {label}
     </button>
   );
