c43e7cc5ebbb66b92fdfaae5bb3da11f384985cb
thewanionly <pelloani@gmail.com>
2024-01-21 01:07:05 +0800

    build: setup shadcn ui

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { twMerge } from 'tailwind-merge';
+import { cn } from '@/utils/styles';
 
 type ButtonProps = React.ComponentProps<'button'> & {
   /* Button's label */
@@ -9,7 +9,7 @@ export const Button = ({ className = '', label, ...props }: ButtonProps) => {
   return (
     <button
       type="button"
-      className={twMerge(
+      className={cn(
         `border-2 border-green bg-green px-16 py-5 uppercase text-grey-dark hover:bg-grey-dark hover:text-green ${className}`
       )}
       {...props}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'tailwindcss';
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  prefix: '',
   theme: {
     colors: {
       green: '#a4ffaf',
@@ -17,12 +19,34 @@ const config: Config = {
       'grey-dark': '#24232c',
       'grey-darkest': '#18171f',
     },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontFamily: {
         'jetbrains-mono': ['var(--font-jetbrains-mono)'],
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [],
-};
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
+
 export default config;
