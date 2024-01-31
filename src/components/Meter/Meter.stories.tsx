8bbaf28b821ed4495ed88362f30f0e884b5fa898
thewanionly <pelloani@gmail.com>
2024-02-01 05:24:44 +0800

    feat: support more states for Meter component

--- src/components/Meter/Meter.stories.tsx
+++ src/components/Meter/Meter.stories.tsx
@@ -23,5 +23,39 @@ type Story = StoryObj<typeof Meter>;
 export const MeterEmpty: Story = {
   args: {
     numOfBars: DEFAULT_NUM_OF_BARS,
+    value: 0,
+    max: 12,
+  },
+};
+
+export const Meter25: Story = {
+  args: {
+    numOfBars: DEFAULT_NUM_OF_BARS,
+    value: 3,
+    max: 12,
+  },
+};
+
+export const Meter50: Story = {
+  args: {
+    numOfBars: DEFAULT_NUM_OF_BARS,
+    value: 6,
+    max: 12,
+  },
+};
+
+export const Meter75: Story = {
+  args: {
+    numOfBars: DEFAULT_NUM_OF_BARS,
+    value: 9,
+    max: 12,
+  },
+};
+
+export const MeterFull: Story = {
+  args: {
+    numOfBars: DEFAULT_NUM_OF_BARS,
+    value: 12,
+    max: 12,
   },
 };
--- src/components/Meter/Meter.tsx
+++ src/components/Meter/Meter.tsx
@@ -14,9 +14,58 @@ type MeterProps = {
   max: number;
 };
 
+// TODO: don't use enums
+enum MeterBarColor {
+  RED = 'red',
+  YELLOW = 'yellow',
+  ORANGE = 'orange',
+  GREEN = 'green',
+}
+
+type MeterBarProps = {
+  filled: boolean;
+  color?: MeterBarColor;
+};
+
+const MeterBar = ({ filled, color }: MeterBarProps) => {
+  const barBgColor = color ?? 'transparent';
+  const barBorderColor = color ?? 'grey-lightest';
+
+  return (
+    <span
+      className={cn(
+        'h-7 w-2.5 border-2',
+        filled ? `border-${barBorderColor}` : 'border-grey-lightest',
+        filled && `bg-${barBgColor}`
+      )}
+      data-testid={filled ? 'meter-bar-filled' : 'meter-bar'}
+    />
+  );
+};
+
 export const Meter = forwardRef<HTMLDivElement, MeterProps>(
   ({ className = '', numOfBars = DEFAULT_NUM_OF_BARS, value, max }, ref) => {
     const classes = cn(`flex gap-2`, className);
+
+    const percentage = (value / max) * 100;
+
+    // TODO: make this better
+    let barColor: MeterBarColor;
+    let barsToFill = 0;
+
+    if (percentage > 0 && percentage <= 25) {
+      barColor = MeterBarColor.RED;
+      barsToFill = Math.floor(numOfBars * 0.25);
+    } else if (percentage > 25 && percentage <= 50) {
+      barColor = MeterBarColor.ORANGE;
+      barsToFill = Math.floor(numOfBars * 0.5);
+    } else if (percentage > 50 && percentage <= 75) {
+      barColor = MeterBarColor.YELLOW;
+      barsToFill = Math.floor(numOfBars * 0.75);
+    } else if (percentage > 75) {
+      barColor = MeterBarColor.GREEN;
+      barsToFill = numOfBars;
+    }
 
     return (
       <div
@@ -28,10 +77,10 @@ export const Meter = forwardRef<HTMLDivElement, MeterProps>(
         aria-valuemin={0}
       >
         {range(numOfBars).map((barIndex) => (
-          <span
+          <MeterBar
             key={barIndex}
-            className="h-7 w-2.5 border-2 border-grey-lightest"
-            data-testid="meter-bar"
+            filled={barsToFill > 0 && barIndex + 1 <= barsToFill}
+            color={barColor}
           />
         ))}
       </div>
