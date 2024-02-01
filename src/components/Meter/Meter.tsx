ef2ca55ce02ee50e1d3634a411515239caa15849
thewanionly <pelloani@gmail.com>
2024-02-02 05:04:35 +0800

    refactor: create mapping for MeterBar level to properties

--- src/components/Meter/Meter.tsx
+++ src/components/Meter/Meter.tsx
@@ -14,31 +14,67 @@ type MeterProps = {
   max: number;
 };
 
-// TODO: don't use enums
-enum MeterBarColor {
-  RED = 'red',
-  YELLOW = 'yellow',
-  ORANGE = 'orange',
-  GREEN = 'green',
+enum MeterBarLevel {
+  EMPTY = 'empty',
+  QUARTER = 'quarter',
+  HALF = 'half',
+  THREE_QUATERS = 'three_quarters',
+  FULL = 'full',
 }
 
-type MeterBarProps = {
-  filled: boolean;
-  color?: MeterBarColor;
+interface MeterBarProperties {
+  bg: string;
+  border: string;
+  filledBars: number | ((numOfBars: number) => number);
+}
+
+const MeterBarLevelMap: Record<MeterBarLevel, MeterBarProperties> = {
+  [MeterBarLevel.EMPTY]: {
+    bg: 'bg-transparent',
+    border: 'border-grey-lightest',
+    filledBars: 0,
+  },
+  [MeterBarLevel.QUARTER]: {
+    bg: 'bg-red',
+    border: 'border-red',
+    filledBars: (numOfBars) => Math.floor(numOfBars * 0.33),
+  },
+  [MeterBarLevel.HALF]: {
+    bg: 'bg-orange',
+    border: 'border-orange',
+    filledBars: (numOfBars) => Math.floor(numOfBars * 0.66),
+  },
+  [MeterBarLevel.THREE_QUATERS]: {
+    bg: 'bg-yellow',
+    border: 'border-yellow',
+    filledBars: (numOfBars) => Math.floor(numOfBars * 0.99),
+  },
+  [MeterBarLevel.FULL]: {
+    bg: 'bg-green',
+    border: 'border-green',
+    filledBars: (numOfBars) => numOfBars,
+  },
 };
 
-const MeterBar = ({ filled, color }: MeterBarProps) => {
-  const barBgColor = color ?? 'transparent';
-  const barBorderColor = color ?? 'grey-lightest';
+type MeterBarProps = {
+  position: number;
+  numOfBars: number;
+  level: MeterBarLevel;
+};
+
+const MeterBar = ({ position, numOfBars, level }: MeterBarProps) => {
+  const { bg, border, filledBars } = MeterBarLevelMap[level];
+
+  const barsToFill = typeof filledBars === 'function' ? filledBars(numOfBars) : filledBars;
+  const isFilled = barsToFill > 0 && position <= barsToFill;
+
+  const bgColor = isFilled ? bg : 'bg-transparent';
+  const borderColor = isFilled ? border : 'border-grey-lightest';
 
   return (
     <span
-      className={cn(
-        'h-7 w-2.5 border-2',
-        filled ? `border-${barBorderColor}` : 'border-grey-lightest',
-        filled && `bg-${barBgColor}`
-      )}
-      data-testid={filled ? 'meter-bar-filled' : 'meter-bar'}
+      className={cn('h-7 w-2.5 border-2', bgColor, borderColor)}
+      data-testid={isFilled ? 'meter-bar-filled' : 'meter-bar'}
     />
   );
 };
@@ -50,21 +86,16 @@ export const Meter = forwardRef<HTMLDivElement, MeterProps>(
     const percentage = (value / max) * 100;
 
     // TODO: make this better
-    let barColor: MeterBarColor;
-    let barsToFill = 0;
+    let level = MeterBarLevel.EMPTY;
 
     if (percentage > 0 && percentage <= 33) {
-      barColor = MeterBarColor.RED;
-      barsToFill = Math.floor(numOfBars * 0.33);
+      level = MeterBarLevel.QUARTER;
     } else if (percentage > 33 && percentage <= 66) {
-      barColor = MeterBarColor.ORANGE;
-      barsToFill = Math.floor(numOfBars * 0.66);
+      level = MeterBarLevel.HALF;
     } else if (percentage > 66 && percentage <= 99) {
-      barColor = MeterBarColor.YELLOW;
-      barsToFill = Math.floor(numOfBars * 0.99);
+      level = MeterBarLevel.THREE_QUATERS;
     } else if (percentage === 100) {
-      barColor = MeterBarColor.GREEN;
-      barsToFill = numOfBars;
+      level = MeterBarLevel.FULL;
     }
 
     return (
@@ -77,11 +108,7 @@ export const Meter = forwardRef<HTMLDivElement, MeterProps>(
         aria-valuemin={0}
       >
         {range(numOfBars).map((barIndex) => (
-          <MeterBar
-            key={barIndex}
-            filled={barsToFill > 0 && barIndex + 1 <= barsToFill}
-            color={barColor}
-          />
+          <MeterBar key={barIndex} position={barIndex + 1} numOfBars={numOfBars} level={level} />
         ))}
       </div>
     );
