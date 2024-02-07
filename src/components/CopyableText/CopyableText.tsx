87c10b83337f87716099cb13586fdb69329e14e2
thewanionly <pelloani@gmail.com>
2024-02-08 03:57:29 +0800

    feat: use ghost Button in CopyableText copy icon

--- src/components/CopyableText/CopyableText.tsx
+++ src/components/CopyableText/CopyableText.tsx
@@ -2,9 +2,9 @@
 
 import * as React from 'react';
 
+import { Button } from '@/components/Button';
+import { Copy } from '@/components/Icon';
 import { cn } from '@/utils/styles';
-
-import { Copy } from '../Icon';
 
 type CopyableTextProps = {
   className?: string;
@@ -14,7 +14,7 @@ type CopyableTextProps = {
 
 const COPIED_DURATION = 2500;
 
-const CopyableText = React.forwardRef<HTMLDivElement, CopyableTextProps>(
+export const CopyableText = React.forwardRef<HTMLDivElement, CopyableTextProps>(
   ({ className = '', value, placeholder }, ref) => {
     const [isCopied, setIsCopied] = React.useState(false);
 
@@ -34,26 +34,29 @@ const CopyableText = React.forwardRef<HTMLDivElement, CopyableTextProps>(
     return (
       <div
         ref={ref}
-        className={cn('flex items-center justify-between bg-grey-dark p-4', className)}
+        className={cn(
+          'flex items-center justify-between bg-grey-dark p-4',
+          !value && 'pointer-events-none',
+          className
+        )}
       >
         <span className={cn('mr-3 flex-1 text-2xl text-grey-lightest', !value && 'opacity-25')}>
           {value || placeholder}
         </span>
         {isCopied && <span className="mr-4 uppercase text-green">copied</span>}
-        <Copy
-          title="copy value"
-          className={cn(
-            'h-5 w-[17.5px] text-green',
-            value ? 'cursor-pointer hover:text-grey-lightest' : 'opacity-25',
-            isCopied && 'text-green hover:text-green'
-          )}
-          onClick={handleCopy}
-        />
+        <Button variant="ghost" onClick={handleCopy} disabled={!value}>
+          <Copy
+            title="copy value"
+            className={cn(
+              'h-5 w-[17.5px] text-green',
+              value ? 'cursor-pointer hover:text-grey-lightest' : 'opacity-25',
+              isCopied && 'text-green hover:text-green'
+            )}
+          />
+        </Button>
       </div>
     );
   }
 );
 
 CopyableText.displayName = 'CopyableText';
-
-export { CopyableText };
