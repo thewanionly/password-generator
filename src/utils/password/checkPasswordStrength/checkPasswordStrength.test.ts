af28b0c041e9dc61481007b3ae853770b6e627b0
thewanionly <pelloani@gmail.com>
2024-02-17 05:31:34 +0800

    test: update checkPasswordStrength testing

--- src/utils/password/checkPasswordStrength/checkPasswordStrength.test.ts
+++ src/utils/password/checkPasswordStrength/checkPasswordStrength.test.ts
@@ -13,28 +13,28 @@ describe('checkPasswordStrength', () => {
 
   describe('different strengths', () => {
     it('returns "too weak" and a value of 1 when password has length of 3 and only one rule is applied', () => {
-      const { strength, value } = checkPasswordStrength('abc');
+      const { strength, value } = checkPasswordStrength('too');
 
       expect(strength).toBe(PasswordStrength.TOO_WEAK);
       expect(value).toBe(1);
     });
 
     it('returns "weak" and a value of 2 when password has length of 6 and only one rule is applied', () => {
-      const { strength, value } = checkPasswordStrength('abcdef');
+      const { strength, value } = checkPasswordStrength('we4K');
 
       expect(strength).toBe(PasswordStrength.WEAK);
       expect(value).toBe(2);
     });
 
     it('returns "medium" and a value of 3 when password has length of 9 and only two rules are applied', () => {
-      const { strength, value } = checkPasswordStrength('abcABCdeF');
+      const { strength, value } = checkPasswordStrength('m3d!uUM');
 
       expect(strength).toBe(PasswordStrength.MEDIUM);
       expect(value).toBe(3);
     });
 
     it('returns "medium" and a value of 4 when password has length of 12 and all rules are applied', () => {
-      const { strength, value } = checkPasswordStrength('abcABC123#$%aB9');
+      const { strength, value } = checkPasswordStrength('Str0n&&6!3sT');
 
       expect(strength).toBe(PasswordStrength.STRONG);
       expect(value).toBe(4);
