57d818f6e01c60208333ea7902254a08bcdd5393
thewanionly <pelloani@gmail.com>
2024-02-23 04:32:05 +0800

    refactor: update stylings related to height of body and main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { DEFAULT_PASSWORD_LENGTH } from '@/constants/password';
 
 export default function Home() {
   return (
-    <main className="mx-auto flex min-h-screen w-4/5 min-w-[375px] max-w-screen-xl items-center justify-center px-4 py-24">
+    <main className="mx-auto my-16 flex w-4/5 min-w-[375px] max-w-screen-xl justify-center md:my-32">
       <PasswordGenerator
         className="shrink-1 grow-0 basis-[540px]"
         initialCharLength={DEFAULT_PASSWORD_LENGTH}
