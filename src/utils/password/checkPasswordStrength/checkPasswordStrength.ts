4f751284868145975d5161d1416f42ff7670215e
thewanionly <pelloani@gmail.com>
2024-03-11 00:00:27 +0800

    refactor: break down into additions and deductions

--- src/utils/password/checkPasswordStrength/checkPasswordStrength.ts
+++ src/utils/password/checkPasswordStrength/checkPasswordStrength.ts
@@ -16,51 +16,40 @@ type CheckPasswordStrengthReturnValue = {
 
 const RECOMMENDED_PASSWORD_LENGTH = 12;
 
-// Logic used here is based from this strength test: https://www.uic.edu/apps/strong-password/
-export const checkPasswordStrength = (password: string): CheckPasswordStrengthReturnValue => {
-  if (!password) return { strength: '', value: 0 };
-
+const calculateAdditions = (password: string): number => {
   const passwordLength = password.length;
-  const upperCaseCount = password.replace(/[^A-Z]/g, '').length;
-  const lowerCaseCount = password.replace(/[^a-z]/g, '').length;
+
+  const upperCaseCount = (password.match(/[A-Z]/g) || []).length;
+  const lowerCaseCount = (password.match(/[a-z]/g) || []).length;
   const numbersCount = (password.match(/\d/g) || []).length;
   const symbolsCount = (password.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g) || []).length;
 
-  let score = 0;
+  let additions = 0;
 
-  /************* ADDITIONS *************/
   // 1. number of chars
-  score += passwordLength * 4;
+  additions += passwordLength * 4;
 
-  // 2. uppercase letters (only add when at least one of the other chars is present and passwordLength > 4)
-  if (
-    passwordLength > 4 &&
-    upperCaseCount > 0 &&
-    lowerCaseCount + numbersCount + symbolsCount > 0
-  ) {
-    score += (passwordLength - upperCaseCount) * 2;
+  // number of letters/numbers  (only add when passwordLength > 4 and at least one of the other chars is present )
+  if (passwordLength > 4) {
+    // 2. uppercase letters
+
+    if (upperCaseCount > 0 && lowerCaseCount + numbersCount + symbolsCount > 0) {
+      additions += (passwordLength - upperCaseCount) * 2;
+    }
+
+    // 3. lowercase letters
+    if (lowerCaseCount > 0 && upperCaseCount + numbersCount + symbolsCount > 0) {
+      additions += (passwordLength - lowerCaseCount) * 2;
+    }
+
+    // 4. numbers
+    if (numbersCount > 0 && upperCaseCount + lowerCaseCount + symbolsCount > 0) {
+      additions += numbersCount * 4;
+    }
   }
 
-  // 3. lowercase letters (only add when at least one of the other chars is present and passwordLength > 4)
-  if (
-    passwordLength > 4 &&
-    lowerCaseCount > 0 &&
-    upperCaseCount + numbersCount + symbolsCount > 0
-  ) {
-    score += (passwordLength - lowerCaseCount) * 2;
-  }
-
-  // 4. numbers (only add when at least one of the other chars is present and passwordLength > 4)
-  if (
-    passwordLength > 4 &&
-    numbersCount > 0 &&
-    upperCaseCount + lowerCaseCount + symbolsCount > 0
-  ) {
-    score += numbersCount * 4;
-  }
-
-  // 4. symbols
-  score += symbolsCount * 6;
+  // 4. symbols count
+  additions += symbolsCount * 6;
 
   // 5. middle numbers or symbols
   const midNumOrSymCount = (
@@ -68,7 +57,7 @@ export const checkPasswordStrength = (password: string): CheckPasswordStrengthRe
       .substring(1, password.length - 1)
       .match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~\d]/g) || []
   ).length;
-  score += midNumOrSymCount * 2;
+  additions += midNumOrSymCount * 2;
 
   // 6. meets the requirments (should only be added once all requirements are met)
   //  [1] at least 12 length
@@ -76,8 +65,6 @@ export const checkPasswordStrength = (password: string): CheckPasswordStrengthRe
   //  [3] 1 lowercase letter
   //  [4] 1 number
   //  [5] 1 symbol
-  const REQUIREMENTS_COUNT = 5;
-
   if (
     passwordLength >= RECOMMENDED_PASSWORD_LENGTH &&
     upperCaseCount > 0 &&
@@ -85,58 +72,82 @@ export const checkPasswordStrength = (password: string): CheckPasswordStrengthRe
     numbersCount > 0 &&
     symbolsCount > 0
   ) {
-    score += REQUIREMENTS_COUNT * 2;
+    const REQUIREMENTS_COUNT = 5;
+    additions += REQUIREMENTS_COUNT * 2;
   }
 
-  /************* DEDUCTIONS *************/
+  return additions;
+};
+
+const calculateDeductions = (password: string): number => {
+  let deductions = 0;
+
   // 1. letters only
-  if (/^[a-zA-Z]+$/.test(password)) score -= upperCaseCount + lowerCaseCount;
+  if (/^[a-zA-Z]+$/.test(password)) {
+    const lettersCount = (password.match(/[a-zA-Z]/g) || []).length;
+    deductions += lettersCount;
+  }
 
   // 2. numbers only
-  if (/^\d+$/.test(password)) score -= numbersCount;
+  if (/^\d+$/.test(password)) {
+    const numbersCount = (password.match(/\d/g) || []).length;
+    deductions += numbersCount;
+  }
 
   // 3. repeated characters (case insenstive)
-  score -= calculateRepeatCharsDeduction(password);
+  deductions += calculateRepeatCharsDeduction(password);
 
   // 4. consecutive uppercase letters
-  const upperCaseConsecutiveCount = countConsecutiveUCLetters(password);
-  score -= upperCaseConsecutiveCount * 2;
+  deductions += countConsecutiveUCLetters(password) * 2;
 
   // 5. consecutive lowercase letters
-  const lowerCaseConsecutiveCount = countConsecutiveLCLetters(password);
-  score -= lowerCaseConsecutiveCount * 2;
+  deductions += countConsecutiveLCLetters(password) * 2;
 
   // 6. consecutive numbers
-  const numberConsecutiveCount = countConsecutiveNumbers(password);
-  score -= numberConsecutiveCount * 2;
+  deductions += countConsecutiveNumbers(password) * 2;
 
   // 7. sequential letters (3+)
-  const sequentialLettersCount = countSequentialLetters(password);
-  score -= sequentialLettersCount * 3;
+  deductions += countSequentialLetters(password) * 3;
 
   // 8. sequential numbers (3+)
-  const sequentialNumbersCount = countSequentialNumbers(password);
-  score -= sequentialNumbersCount * 3;
+  deductions += countSequentialNumbers(password) * 3;
 
   // 9. sequential symbols (3+)
-  const sequentialSymbolCount = countSequentialSymbols(password);
-  score -= sequentialSymbolCount * 3;
+  deductions += countSequentialSymbols(password) * 3;
+
+  return deductions;
+};
+
+const SCORE_TO_STRENGTH = [
+  {
+    condition: (score: number) => score >= 90,
+    result: { strength: PasswordStrength.STRONG, value: 4 },
+  },
+  {
+    condition: (score: number) => score < 90 && score >= 60,
+    result: { strength: PasswordStrength.MEDIUM, value: 3 },
+  },
+  {
+    condition: (score: number) => score < 60 && score >= 30,
+    result: { strength: PasswordStrength.WEAK, value: 2 },
+  },
+  {
+    condition: (score: number) => score < 30,
+    result: { strength: PasswordStrength.TOO_WEAK, value: 1 },
+  },
+];
+
+// Logic used here is based from this strength test: https://www.uic.edu/apps/strong-password/
+export const checkPasswordStrength = (password: string): CheckPasswordStrengthReturnValue => {
+  if (!password) return { strength: '', value: 0 };
+
+  const additions = calculateAdditions(password);
+  const deductions = calculateDeductions(password);
 
   // final score (capped with a min of 0 and max of 100)
-  score = Math.min(Math.max(score, 0), 100);
+  const score = Math.min(Math.max(additions - deductions, 0), 100);
 
-  /* Calculate strength */
-  if (score >= 90) {
-    return { strength: PasswordStrength.STRONG, value: 4 };
-  }
-
-  if (score < 90 && score >= 60) {
-    return { strength: PasswordStrength.MEDIUM, value: 3 };
-  }
-
-  if (score < 60 && score >= 30) {
-    return { strength: PasswordStrength.WEAK, value: 2 };
-  }
-
-  return { strength: PasswordStrength.TOO_WEAK, value: 1 };
+  // calculate strength  from score
+  return SCORE_TO_STRENGTH.find(({ condition }) => condition(score))
+    ?.result as CheckPasswordStrengthReturnValue;
 };
